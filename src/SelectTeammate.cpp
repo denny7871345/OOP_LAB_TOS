4c0f344e0b8e97ff0843f474755fac3801671111
denny7871345 <90140271+denny7871345@users.noreply.github.com>
2024-05-24 12:10:20 +0800

    0524

--- src/SelectTeammate.cpp
+++ src/SelectTeammate.cpp
@@ -1,9 +1,6 @@
 //
 // Created by fungp on 2024/5/24.
 //
-#ifndef SELECTTEAMMATE_HPP
-#define SELECTTEAMMATE_HPP
-
 #include "iostream"
 #include "memory"
 #include "vector"
@@ -15,10 +12,21 @@ std::vector<std::shared_ptr<Member>> Team::GetTeam() {
     return m_MemberList;
 }
 
-void Team::LoadMemberFile() {
+void Team::LoadMemberFile(MemberSettingData target) {
     m_Data = target;
 }
 
 NorthTeam::NorthTeam() {
-    //std::shared_ptr<Member> Loki = std::make_shared<Loki>();
+    std::shared_ptr<Member> token = std::make_shared<Loki>(m_Data);
+    m_MemberList.push_back(token);
+    token = std::make_shared<DarkBeast>(m_Data);
+    m_MemberList.push_back(token);
+    token = std::make_shared<Baphomet>(m_Data);
+    m_MemberList.push_back(token);
+    token = std::make_shared<DaJi>(m_Data);
+    m_MemberList.push_back(token);
+    token = std::make_shared<Artemis>(m_Data);
+    m_MemberList.push_back(token);
+    token = std::make_shared<Loki>(m_Data);
+    m_MemberList.push_back(token);
 }
